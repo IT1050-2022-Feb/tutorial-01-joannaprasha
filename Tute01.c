8a7eaac315b499a92d8b969a63567053f456fda2
Joanna <88624291+joannaprasha@users.noreply.github.com>
2022-02-16 13:28:43 +0000

    Tutorial1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,21 @@
 #include <stdio.h>
 
 int main() {
-  
+
+  int mark1,mark2;
+  int sum=0;
+  float average=0;
+
+  printf("Enter mark1: ");
+  scanf("%d", &mark1);
+
+  printf("Enter mark2: ");
+  scanf("%d", &mark2);
+
+  sum=mark1+ mark2;
+  average= sum/2;
+
+  printf("The average mark is: %.2f",average);
   return 0;
 }
 
--- Tute03.c
+++ Tute03.c
@@ -14,6 +14,18 @@ sum = 1+2+3+...+10 = 55 */
 #include <stdio.h>
 int main() {
   
+  int n;
+  int sum=0;
+  int i;
+
+  printf("Enter the number: ");
+  scanf("%d", &n);
+  
+  for(i=0; i<=n; i++)
+    {
+      sum=sum+i;
+    }
+  printf("The sum is: %d", sum);
   return 0;
 }
 
